06759f0b6662f6be3b5405d62d12ec51c8f56b75
Paul Cothenet <paul@madkudu.com>
2017-05-18 09:39:43 -0700

    Perf optimization

--- index.js
+++ index.js
@@ -3,37 +3,36 @@
 const jsesc = require('jsesc')
 const debug = require('debug')('json-line-parser')
 
-/* @todo: use an error rate instead */
 const MAX_ERROR = 50
-
-var error_counter = 0
+let error_counter = 0
 
 const handle_error = function (error, line, skip_errors) {
   error_counter += 1
   debug(line)
-  if (error && error_counter >= MAX_ERROR && !skip_errors) {
+  if (error && error_counter >= MAX_ERROR && !skip_errors) { // @TODO: turn this into an error rate?
     throw new Error('Reached max number of errors')
   }
 }
 
 const standard_parser = function (line) {
-  try {
-    if (line.length > 0) {
+  if (line.length > 0) {
+    try {
       return JSON.parse(line)
+    } catch (e) {
+      handle_error(e, line, false)
+      return undefined
     }
-  } catch (e) {
-    handle_error(e, line, false)
-    return {}
   }
 }
 
+// this is probably an optimization killer, use parsimoniously
 const kissmetrics_parser = function (line) {
-  try {
-    if (line.length > 0) {
+  if (line.length > 0) {
+    try {
       return JSON.parse(jsesc(line).replace("\\'", "'"))
+    } catch (e) {
+      return undefined
     }
-  } catch (e) {
-    return {}
   }
 }
 
@@ -41,8 +40,7 @@ const kissmetrics_parser = function (line) {
 * @param {object} [options]
 * @param {object} [options.kissmetrics] set to true to use a kissmetrics specific parser
 */
-module.exports = function (options) {
-  options = options || {}
+module.exports = function (options = {}) {
   if (options.kissmetrics) {
     return kissmetrics_parser
   } else {
